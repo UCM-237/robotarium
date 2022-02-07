70ff84a6ff64e7a8b23932104b87cb03eb2060af
Hector Garcia de Marina <noeth3r@gmail.com>
2022-02-07 14:40:19 +0100

    log python

---
+++ algoritmo/python/postpro.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as pl
+import numpy as np
+from numpy import linalg as la
+
+r1_log = np.loadtxt("./experiment1/logRP1_Coordenadas.txt", delimiter=',', skiprows = 2, max_rows = 1)
